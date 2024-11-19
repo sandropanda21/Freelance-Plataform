d357fbe64ef99dd5d62eb64aaba51ffa7aec7023
sandropanda21 <sandropanda0821@gmail.com>
2024-11-19 16:31:58 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/users', (req, res) => {
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.listen(3000)
